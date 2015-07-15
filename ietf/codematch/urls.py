0ea67e282d43a40c62f75660c09aa993dd455b53
Matheus Pfitscher <matheuspfitscher@Matiass-Mini.inf.ufrgs.br>
2015-07-15 15:59:00 -0300

    Adicionando botões para codematches

--- ietf/codematch/urls.py
+++ ietf/codematch/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     url(r'^showlist/$', views.showlist, name="showlist"),
     url(r'^about/$', views.about , name="about"),
     url(r'^new/$', views.new , name="new"),
+    url(r'^search/$', views.search, name="search"),
     url(r'^(?P<pk>[0-9]+)/$', views.show, name='show'),
 )
