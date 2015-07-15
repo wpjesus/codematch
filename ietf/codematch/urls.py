5dd3e38984e0de444bc6578ad76b7e8332ae16c1
Matheus Pfitscher <matheuspfitscher@Matiass-Mini.inf.ufrgs.br>
2015-07-15 15:44:50 -0300

    estava faltando arquivo urls.py

--- ietf/codematch/urls.py
+++ ietf/codematch/urls.py
@@ -5,9 +5,9 @@ from django.core.urlresolvers import reverse_lazy
 from ietf.codematch import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.showlist, name="showlist"),
+    url(r'^$', views.index, name="index"),
+    url(r'^showlist/$', views.showlist, name="showlist"),
     url(r'^about/$', views.about , name="about"),
     url(r'^new/$', views.new , name="new"),
-    url(r'^search/$', views.search, name="search"),
     url(r'^(?P<pk>[0-9]+)/$', views.show, name='show'),
 )
