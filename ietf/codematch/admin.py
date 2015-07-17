74e8011a79514b345b13cd7b20717c374f4ce448
Matheus Pfitscher <matheuspfitscher@Matiass-Mini.inf.ufrgs.br>
2015-07-17 16:00:56 -0300

    removendo arquivos que não existem mais

--- ietf/codematch/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import ProjectContainer, CodeRequest, CodingProject
-
-admin.site.register(ProjectContainer)
-admin.site.register(CodingProject)
