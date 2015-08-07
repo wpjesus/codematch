d233d95411321300edaf56aacfe2f7ec12dd25d2
Matheus Pfitscher <matheuspfitscher@Matiass-Mini.inf.ufrgs.br>
2015-08-07 13:54:44 -0300

    fix add project container without coderequest

--- ietf/codematch/matches/urls.py
+++ ietf/codematch/matches/urls.py
@@ -7,7 +7,7 @@ from ietf.codematch.matches import views
 urlpatterns = patterns('ietf.codematch.matches.views',
     url(r'^showlist/$', views.showlist, name="showlist"),
     url(r'^new/$', views.new , name="new"),
-    url(r'^new/(?P<pk>[0-9]+)/$', views.new , name="new"),
+    url(r'^new/(?P<pk>.*)/$', views.new , name="new"),
     url(r'^search/$', views.search, name="search"),
     url(r'^(?P<pk>[0-9]+)/(?P<ck>[0-9]+)$', views.show, name='show'),
 )
